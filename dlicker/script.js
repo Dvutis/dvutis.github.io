4f8961bc48e678b017588495fdd1232e81b60bb3
Dvutis <81529749+Dvutis@users.noreply.github.com>
2021-03-29 13:10:09 +0300

    Update script.js

--- dlicker/script.js
+++ dlicker/script.js
@@ -1,38 +1,43 @@
+let profil = document.getElementById("profil")
+let referals = document.getElementById("referals")
+let clicker = document.getElementById("clicker")
+let shop = document.getElementById("shop")
+let rules = document.getElementById("rules")
 let content1 = document.getElementById("content1")
 let content2 = document.getElementById("content2")
 let content3 = document.getElementById("content3")
 let content4 = document.getElementById("content4")
 let content5 = document.getElementById("content5")
 
-content1.addEventListener("click", () => {
+profil.addEventListener("click", () => {
     content1.style.display = "block"
     content2.style.display = "none"
     content3.style.display = "none"
     content4.style.display = "none"
     content5.style.display = "none"
 })
-content2.addEventListener("click", () => {
+referals.addEventListener("click", () => {
     content1.style.display = "none"
     content2.style.display = "block"
     content3.style.display = "none"
     content4.style.display = "none"
     content5.style.display = "none"
 })
-content3.addEventListener("click", () => {
+clicker.addEventListener("click", () => {
     content1.style.display = "none"
     content2.style.display = "none"
     content3.style.display = "block"
     content4.style.display = "none"
     content5.style.display = "none"
 })
-content4.addEventListener("click", () => {
+shop.addEventListener("click", () => {
     content1.style.display = "none"
     content2.style.display = "none"
     content3.style.display = "none"
     content4.style.display = "block"
     content5.style.display = "none"
 })
-content5.addEventListener("click", () => {
+rules.addEventListener("click", () => {
     content1.style.display = "none"
     content2.style.display = "none"
     content3.style.display = "none"
